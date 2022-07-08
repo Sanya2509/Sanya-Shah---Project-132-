ec27c893023178ccd98f851936eab2c208573291
Sanya2509 <83489823+Sanya2509@users.noreply.github.com>
2022-07-08 21:55:45 +0530

    Add files via upload

---
+++ Bottle.js
@@ -0,0 +1,30 @@
+Status = "";
+bot_image = "";
+
+function preload(){
+    bot_image = loadImage("bot.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(bot_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(bot_image,0,0,640,350);
+}
